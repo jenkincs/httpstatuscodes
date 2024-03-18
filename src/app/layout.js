34fe9ce2cc72459a9e26fa745950d4fc1f4e331d
jenkincsx <jenkincstech@gmail.com>
2024-03-18 22:10:27 +0800

    update metadata info

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,9 @@ import Header from "@/components/header/header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Http Status Codes",
-  description: "A list of HTTP status codes and their meanings.",
+  title: "HTTP Status Codes: A Comprehensive Guide for Beginners",
+  description: "Learn about different HTTP status codes and their meanings. Understand the importance of HTTP status codes in web development and troubleshooting. Find everything you need to know about HTTP status codes in this comprehensive guide.",
+  keywords: "HTTP status codes, 100 Continue, 200 OK, 201 Created, 401 Unauthorized, 403 Forbidden, 404 Not Found, 500 Internal Server Error, 502 Bad Gateway, 504 Gateway Timeout, web development, troubleshooting",
 };
 
 export default function RootLayout({ children }) {
