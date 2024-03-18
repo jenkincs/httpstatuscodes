3153bf5eed562a69cd3bc684cad269cebd9106e3
jenkincsx <jenkincstech@gmail.com>
2024-03-18 22:19:59 +0800

    update metadata info

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "HTTP Status Codes: A Comprehensive Guide for Beginners",
-  description: "Learn about different HTTP status codes and their meanings. Find everything you need to know about HTTP status codes",
+  description: "Learn about different HTTP status codes and their meanings, usages and samples. Find everything you need to know about HTTP status codes",
   keywords: "HTTP status codes, 200 OK, 404 Not Found, 500 Internal Server Error, web development, troubleshooting",
 };
 
